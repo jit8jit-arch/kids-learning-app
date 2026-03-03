8f6277863e5fb621f4029bf98db606be7cee4d67
jit8jit-arch <jit8jit@gmail.com>
2026-03-04 01:43:08 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
+const CACHE_NAME = 'kids-tracer-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
     e.waitUntil(
-      caches.open('kids-tracer').then((cache) => cache.addAll([
-        '/',
-        '/index.html',
-        '/app.js',
-        '/manifest.json'
-      ]))
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
     );
 });
   
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
+
+// Clear out the old, broken caches
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
     );
 });
